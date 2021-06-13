5ef03fd027f2110a963d9b4bc24f3dffc7ceac81
niravvaishnav <85818175+niravvaishnav@users.noreply.github.com>
2021-06-13 11:05:47 +0530

    Task - 1(Data Structures)
    A program which accepts the radius of a circle from the user and computes area.

---
+++ radius.py
@@ -0,0 +1,6 @@
+
+# A program which accepts the radius of a circle from the user and computes area.  
+
+from math import pi
+r = float(input('Input the radius of the circle: '))
+print('The Area of the circle is:', pi*r**2)
