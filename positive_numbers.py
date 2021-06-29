ad62a50e98aecbefd3154ab242e21de2b6acbf58
niravvaishnav <85818175+niravvaishnav@users.noreply.github.com>
2021-06-29 11:41:51 +0530

    Add files via upload

---
+++ positive_numbers.py
@@ -0,0 +1,16 @@
+
+# A program to print all positive numbers in a range.
+
+list1 = [12, -7, 5, 64, -14]
+
+for i in list1:
+    if i > 0:
+        print(i, end = ' ')
+
+list2 = [12, 14, -95, 3]
+l2 = []
+
+for i in list2:
+    if i > 0:
+        l2.append(i)
+print(l2)
